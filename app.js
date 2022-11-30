ff380d6a9bd342234479d34434e6565bb6dfb9f1
Danisole <danisole82@gmail.com>
2022-11-30 00:38:35 -0300

    estilos de tablas

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-let app = new Vue({
+const app = new Vue({
     el:'#app',
     data:{
-        clientes:[
+        empleados:[
+            {
+                nombre: "cristina diaz",
+                direccion: "mitre 567",
+                dni: "28646908"
+            },
+            {
+                nombre: "vicente Lopez",
+                direccion: "mendoza 344",
+                dni: "17334905"
+            },
+            {
+                nombre: "pablo castro",
+                direccion: "colombia 2432",
+                dni: "29575983"
+            }
+
+        ],
+        empleados2:[
+            {
+                nombre: "maria morales",
+                direccion: "avenida libertador 1233",
+                dni: "36858908"
+            },
+            {
+                nombre: "carla rojas",
+                direccion: "Calle verdadera 233",
+                dni: "25834905"
+            },
+            {
+                nombre: "julio iglesias",
+                direccion: "rawson 1233",
+                dni: "27747483"
+            }
+
+        ],
+        empleados3:[
             {
                 nombre: "Juan Perez",
                 direccion: "avenida siempreviva 1233",
@@ -15,10 +51,14 @@ let app = new Vue({
             {
                 nombre: "Pepe Argento",
                 direccion: "Rivadavia 1233",
-                dni: 28745983
+                dni: "28745983"
             }
 
-        ]
+        ],
+        estilo1:"text-primary",
+        estilo2:"text-warning",
+        estilo3:"text-success"
+          
     }
 
     
--- tabla.js
+++ tabla.js
@@ -1,10 +1,15 @@
 Vue.component('tabla',{
-    props:{
-        clientes: Array
+    props:{ 
+        empleados: Array,
+        estilo:String,
+        
+        
     }, 
     template:/*html*/`
+    
         <div>
-        <table class="content-table"> 
+        <h3 :class="estilo">tablas de empleados</h3>
+        <table class="table table-striped table-bordered border-primary w-50"> 
             <thead>
                 <tr> 
                     <th>Nombre y Apellido</th> 
@@ -12,16 +17,55 @@ Vue.component('tabla',{
                     <th>Documento</th>
                 </tr> 
             </thead> 
-            <tbody > 
-                <tr class="table1" v-for="(cliente, index) in clientes" :key="index"> 
-                    <td>{{cliente.nombre}}</td>
-                    <td>{{cliente.direccion}}</td> 
-                    <td>{{cliente.dni}}</td>
+            <tbody  > 
+                <tr class="table1" v-for="(empleado, index) in empleados" :key="index"> 
+                    <td>{{empleado.nombre}}</td>
+                    <td>{{empleado.direccion}}</td> 
+                    <td>{{empleado.dni}}</td>
                 </tr> 
             </tbody> 
         </table>
-       
         </div>
+
+        <div>
+        <table class="table table-striped table-bordered border-primary w-50"> 
+        <thead>
+            <tr> 
+                <th>Nombre y Apellido</th> 
+                <th>menu</th> 
+                <th>horario laboral</th>
+            </tr> 
+        </thead> 
+        <tbody> 
+                <tr class="table2" v-for="(empleado, index) in empleados" :key="index"> 
+                <td>{{empleado.nombre}}</td>
+                <td>{{empleado.direccion}}</td> 
+                <td>{{empleado.dni}}</td>
+                </tr> 
+        </tbody> 
+        </table>
+        </div>
+
+        
+        <div>
+        <table class="table table-striped table-bordered border-primary w-50"> 
+            <thead>
+                <tr> 
+                    <th>Nombre y Apellido</th> 
+                    <th>Seniority</th> 
+                    <th>experiencia</th>
+                </tr> 
+            </thead> 
+            <tbody > 
+                <tr class="table3" v-for="(empleado, index) in empleados" :key="index"> 
+                    <td>{{empleado.nombre}}</td>
+                    <td>{{empleado.direccion}}</td> 
+                    <td>{{empleado.dni}}</td>
+                </tr> 
+            </tbody> 
+        </table>
+        </div>   
+        
 
     `
 })
